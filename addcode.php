4d3a61cb56a3913d6c9183b54bf74b668aef892b
ojSeyi <1509941@rgu.ac.uk>
2016-04-30 01:14:33 +0100

    1st commit

--- addcode.php
+++ addcode.php
@@ -11,7 +11,7 @@ include ("db_connection.php");
     if(empty($_POST['bugName']) || empty($_POST['category']) || empty($_POST['summary'])){
         echo "Enter all values";
     }else {
-        $bugname = $_POST['bugname'];
+        $bugname = $_POST['bugName'];
         $bugcategory = $_POST['category'];
         $bugsummary = $_POST['summary'];
 
