c337076593ae580038cf74c6dd0f391360c62d94
ojSeyi <1509941@rgu.ac.uk>
2016-05-03 22:52:58 +0100

    1st commit

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 1           <h1>BugTracker</h1>
             <form action="login.php" method="post">
                 <label>Username</label>
-                <input type="text" name="username"/>
+                <input type="text" name="username"/><br>
                 <label>Password</label>
                 <input type="text" name="password"/>
             </form>
