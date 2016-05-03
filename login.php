87f954d4f91c2cd0a22df6c333047b408c30c785
ojSeyi <1509941@rgu.ac.uk>
2016-05-03 23:42:02 +0100

    1st commit

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        $sql = "SELECT * FROM users WHERE username = $username AND password = $password";
+        $sql = "SELECT uid FROM users WHERE username = $username AND password = $password";
         $result = mysqli_query($db, $sql);
         if(mysqli_num_rows($result)==1){
             header("Location: index.php");
